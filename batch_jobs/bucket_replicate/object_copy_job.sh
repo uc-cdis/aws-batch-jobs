5611ba3afcddc5503533f0cd17007c3c3cb49733
Edward Malinowski <edwardmalinowski@Edwards-MacBook-Pro.local.cdis>
2020-06-26 09:41:20 -0500

    feat(bucket-replicate): Added bucket replicate batch job

--- batch_jobs/bucket_replicate/object_copy_job.sh
+++ batch_jobs/bucket_replicate/object_copy_job.sh
@@ -1,10 +1,5 @@
 #/bin/bash
 
-
-env
-echo $ACCESS_KEY_ID
-echo $SECRET_ACCESS_KEY
-
 aws configure set aws_access_key_id $ACCESS_KEY_ID
 aws configure set aws_secret_access_key $SECRET_ACCESS_KEY
 
