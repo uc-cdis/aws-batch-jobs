3780960172b7bdf723fe67a8a4e066cb455d0bd1
Edward Malinowski <edwardmalinowski@Edwards-MacBook-Pro.local.cdis>
2020-10-01 07:40:20 -0500

    test(dcf-fix): modified script to fix dcf object permissions

--- batch_jobs/bucket_replicate/object_copy_job.sh
+++ batch_jobs/bucket_replicate/object_copy_job.sh
@@ -2,7 +2,7 @@
 
 aws configure set aws_access_key_id $ACCESS_KEY_ID_ORIGINAL
 aws configure set aws_secret_access_key $SECRET_ACCESS_KEY_ORIGINAL
-`
+
 aws s3api put-object-acl --bucket $bucket --key "$key" --acl bucket-owner-full-control
 
 aws configure set aws_access_key_id $ACCESS_KEY_ID_DESTINATION
