3ee5afcb7a5eba6e07ebb3803393bb144085a914
Edward Malinowski <edwardmalinowski@Edwards-MacBook-Pro.local.cdis>
2020-10-13 12:55:48 -0500

    feat(cleversafe-copy): Added job to copy data to cleversafe and calculate metadata

--- batch_jobs/copy_to_cleversafe/object_copy.sh
+++ batch_jobs/copy_to_cleversafe/object_copy.sh
@@ -7,12 +7,14 @@ gcloud auth activate-service-account --key-file /home/ubuntu/google-sa.json
 gcloud config set project $PROJECT
 mkdir /home/ubuntu/file
 gsutil -u $PROJECT cp gs://$SOURCE_BUCKET/$KEY /home/ubuntu/file/
-fileName=$(ls /home/ubuntu/file)
+fileName=$(ls /home/ubuntu/file/)
 md5=$(md5sum /home/ubuntu/file/$fileName)
+echo $md5
 size=$(wc -c /home/ubuntu/file/$fileName)
+echo $size
 QUEUE_URL=$(aws sqs get-queue-url --queue-name=$SQS_NAME |jq -r .QueueUrl)
 #send sqs message
-aws sqs send-message --region us-east-1 --endpoint-url https://sqs.us-east-1.amazonaws.com/ --queue-url $QUEUE_URL --message-body "{'url': gs://$SOURCE_BUCKET/$KEY, 'md5': $md5, 'size': $size}"
+aws sqs send-message --region us-east-1 --endpoint-url https://sqs.us-east-1.amazonaws.com/ --queue-url $QUEUE_URL --message-body "{\"url\": \"gs://$SOURCE_BUCKET/$KEY\", \"md5\": \"$md5\", \"size\": \"$size\"}"
 
 aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID_CLEVERSAFE
 aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY_CLEVERSAFE
