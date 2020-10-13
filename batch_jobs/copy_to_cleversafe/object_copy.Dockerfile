2b7e7274e1894d0b02a775e03a189df84925ec0b
Edward Malinowski <edwardmalinowski@Edwards-MacBook-Pro.local.cdis>
2020-10-13 11:17:52 -0500

    feat(cleversafe-copy): Added job to copy data to cleversafe and calculate metadata

--- batch_jobs/copy_to_cleversafe/object_copy.Dockerfile
+++ batch_jobs/copy_to_cleversafe/object_copy.Dockerfile
@@ -1,10 +1,11 @@
 FROM amazon/aws-cli
-COPY . /copy_to_cleversafe
+WORKDIR /home/ubuntu
+COPY . /home/ubuntu/copy_to_cleversafe
 
 RUN wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-313.0.1-linux-x86_64.tar.gz
 RUN tar -xvf google-cloud-sdk-313.0.1-linux-x86_64.tar.gz
 RUN ./google-cloud-sdk/install.sh --command-completion true --path-update true --usage-reporting false -q
 
-WORKDIR /copy_to_cleversafe
+WORKDIR /home/ubuntu/copy_to_cleversafe
 ENTRYPOINT ["/bin/bash"]
 CMD ["batch_jobs/copy_to_cleversafe/object_copy.sh" ]
--- batch_jobs/copy_to_cleversafe/object_copy.sh
+++ batch_jobs/copy_to_cleversafe/object_copy.sh
@@ -1,15 +1,15 @@
 #/bin/bash
 aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID_SQS
 aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY_SQS
-aws s3 cp s3://$TEMP_BUCKET/sa.json /google-sa.json
+aws s3 cp s3://$TEMP_BUCKET/sa.json /home/ubuntu/google-sa.json
 
-gcloud auth activate-service-account --key-file /google-sa.json
+gcloud auth activate-service-account --key-file /home/ubuntu/google-sa.json
 gcloud config set project $PROJECT
-mkdir file
-gsutil -u $PROJECT cp gs://$SOURCE_BUCKET/$KEY ./file/
-fileName=$(ls file)
-md5=$(md5sum file/$fileName)
-size=$(wc -c file/$fileName)
+mkdir /home/ubuntu/file
+gsutil -u $PROJECT cp gs://$SOURCE_BUCKET/$KEY /home/ubuntu/file/
+fileName=$(ls /home/ubuntu/file)
+md5=$(md5sum /home/ubuntu/file/$fileName)
+size=$(wc -c /home/ubuntu/file/$fileName)
 local QUEUE_URL=$(aws sqs get-queue-url --queue-name=$SQS_NAME |jq -r .QueueUrl)
 #send sqs message
 aws sqs send-message --region us-east-1 --endpoint-url https://sqs.us-east-1.amazonaws.com/ --queue-url $QUEUE_URL --message-body "{'url': gs://$SOURCE_BUCKET/$KEY, 'md5': $md5, 'size': $size}"
@@ -17,4 +17,4 @@ aws sqs send-message --region us-east-1 --endpoint-url https://sqs.us-east-1.ama
 aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID_CLEVERSAFE
 aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY_CLEVERSAFE
 
-aws s3 cp ./file/$fileName s3://$DESTINATION_BUCKET/$KEY --endpoint-url=https://bionimbus-objstore-cs.opensciencedatacloud.org/
+aws s3 cp /home/ubuntu/file/$fileName s3://$DESTINATION_BUCKET/$KEY --endpoint-url=https://bionimbus-objstore-cs.opensciencedatacloud.org/
