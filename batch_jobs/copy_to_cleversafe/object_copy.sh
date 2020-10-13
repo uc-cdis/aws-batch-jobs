575f18baa5d091862f711355fcce20f9950e54f5
Edward Malinowski <edwardmalinowski@Edwards-MacBook-Pro.local.cdis>
2020-10-13 12:02:41 -0500

    feat(cleversafe-copy): Added job to copy data to cleversafe and calculate metadata

--- batch_jobs/copy_to_cleversafe/object_copy.sh
+++ batch_jobs/copy_to_cleversafe/object_copy.sh
@@ -10,11 +10,11 @@ gsutil -u $PROJECT cp gs://$SOURCE_BUCKET/$KEY /home/ubuntu/file/
 fileName=$(ls /home/ubuntu/file)
 md5=$(md5sum /home/ubuntu/file/$fileName)
 size=$(wc -c /home/ubuntu/file/$fileName)
-local QUEUE_URL=$(aws sqs get-queue-url --queue-name=$SQS_NAME |jq -r .QueueUrl)
+QUEUE_URL=$(aws sqs get-queue-url --queue-name=$SQS_NAME |jq -r .QueueUrl)
 #send sqs message
 aws sqs send-message --region us-east-1 --endpoint-url https://sqs.us-east-1.amazonaws.com/ --queue-url $QUEUE_URL --message-body "{'url': gs://$SOURCE_BUCKET/$KEY, 'md5': $md5, 'size': $size}"
 
 aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID_CLEVERSAFE
 aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY_CLEVERSAFE
 
-aws s3 cp /home/ubuntu/file/$fileName s3://$DESTINATION_BUCKET/$KEY --endpoint-url=https://bionimbus-objstore-cs.opensciencedatacloud.org/
+aws s3 cp /home/ubuntu/file/$fileName s3://$DESTINTION_BUCKET/$KEY --endpoint-url=https://bionimbus-objstore-cs.opensciencedatacloud.org/
