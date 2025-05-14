661b99a1b630262d55a93033e0d270ef40a51ebf
Ed <emalinowski@uchicago.edu>
2025-05-14 14:35:56 -0500

    feat(dcf-datareplicate): testing dcf data replication flow

--- batch_jobs/bucket_replicate/object_copy_job.sh
+++ batch_jobs/bucket_replicate/object_copy_job.sh
@@ -3,7 +3,7 @@
 aws configure set aws_access_key_id $ACCESS_KEY_ID
 aws configure set aws_secret_access_key $SECRET_ACCESS_KEY
 
-./mount-s3 $DESTINATION_BUCKET ./mnt
+/bucket-replicate/mount-s3 $DESTINATION_BUCKET ./mnt
 
 # Copy the file locally from the GDC bucket
 echo "testfile" > ./mnt/$KEY
