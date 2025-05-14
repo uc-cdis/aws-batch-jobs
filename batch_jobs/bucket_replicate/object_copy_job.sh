bf063bee5b7bb9b45392303838474f2b1b18f2e1
Ed <emalinowski@uchicago.edu>
2025-05-14 16:28:57 -0500

    feat(dcf-datareplicate): testing dcf data replication flow

--- batch_jobs/bucket_replicate/object_copy_job.sh
+++ batch_jobs/bucket_replicate/object_copy_job.sh
@@ -5,7 +5,7 @@ aws configure set aws_secret_access_key $SECRET_ACCESS_KEY
 
 mount-s3 $DESTINATION_BUCKET ./mnt
 
-mkdir -p "./mnt/$(dirname "$DESTINATION_BUCKET")"
+mkdir -p "./mnt/$(dirname "$KEY")"
 
 # Copy the file locally from the GDC bucket
 echo "testfile" > ./mnt/$KEY
