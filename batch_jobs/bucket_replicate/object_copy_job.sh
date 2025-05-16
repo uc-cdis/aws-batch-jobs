f04a59fc8a831954c88c1b06638bb3b11a5d7910
Edward Malinowski <emalinowski@uchicago.edu>
2025-05-16 13:41:44 -0500

    Update object_copy_job.sh

--- batch_jobs/bucket_replicate/object_copy_job.sh
+++ batch_jobs/bucket_replicate/object_copy_job.sh
@@ -3,7 +3,7 @@
 aws configure set aws_access_key_id $ACCESS_KEY_ID
 aws configure set aws_secret_access_key $SECRET_ACCESS_KEY
 
-mount-s3 $DESTINATION_BUCKET ./mnt --allow-overwrite flag
+mount-s3 --allow-overwrite $DESTINATION_BUCKET ./mnt
 
 mkdir -p "./mnt/$(dirname "$KEY")"
 
