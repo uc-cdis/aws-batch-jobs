d6dc20dad20212162073ca1898ca5008d321d615
BinamB <numb.baj@gmail.com>
2026-05-21 14:31:21 -0500

    check fileid

--- batch_jobs/dcf_replication/file_get_upload.py
+++ batch_jobs/dcf_replication/file_get_upload.py
@@ -1,6 +1,7 @@
 import argparse
 import hashlib
 import math
+import re
 import sys
 import time
 import urllib
@@ -30,7 +31,16 @@ def api_to_bucket_copy(
     chunk_size,
     retries_num,
 ):
-
+    regex = re.compile(
+        "^[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12}\Z",
+        re.I,
+    )
+    match = regex.match(file_id)
+    if bool(match) == False:
+        print(
+            f"file_id does not match uuid4 pattern. Please send a valid uuid4. {file_id}"
+        )
+        return
     DATA_ENDPOINT = f"https://api.gdc.cancer.gov/data/{file_id}"
 
     s3 = boto3.client("s3")
