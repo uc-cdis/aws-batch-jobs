c595c666401b0eeaa6a2de8f14209c4292a715e9
BinamB <numb.baj@gmail.com>
2026-06-17 16:59:28 -0500

    Add raw string for regex

--- batch_jobs/dcf_replication/file_get_upload.py
+++ batch_jobs/dcf_replication/file_get_upload.py
@@ -32,7 +32,7 @@ def api_to_bucket_copy(
     retries_num,
 ):
     regex = re.compile(
-        "^[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12}\Z",
+        r"^[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12}\Z",
         re.I,
     )
     match = regex.match(file_id)
